f01fd58ed90bdeb59aca953e9274f3cbe39a91ce
Tim <timschngns@aol.com>
2020-05-07 10:44:53 -0400

    Raw Timer

--- Timer.py
+++ Timer.py
@@ -12,7 +12,7 @@ seconds = float()
 minutes = int()
 hours   = int()
 
-run = input("Enter R to run the program")   # Start the program
+run = input("Enter R to run the program:")   # Start the program
 while run.lower()=="r": 
     if seconds > 59: 
         seconds = 0 
@@ -21,6 +21,6 @@ while run.lower()=="r":
         minutes = 0
         hours = hours+1
     os.system('cls')    # Clear the command prompt
-    seconds = (seconds+ .1)    # Count seconds
-    print(hours,":",minutes,":",second)    
-    time.sleep(.1)             
+    seconds = (seconds+ 1)    # Count seconds
+    print(hours,":",minutes,":",seconds)    
+    time.sleep(1)    # Go back to line 17 and repeat while loop
